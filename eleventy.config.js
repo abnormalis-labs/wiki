169ec5aad5a8dd8fe48f442b249ca18e1bff8c71
Jon Kulton <jonkulton@gmail.com>
2025-08-16 21:08:16 -0400

    initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,45 @@
+import { DateTime } from "luxon";
+import _ from "lodash";
+
+import entries from "./_data/entries.json" with { type: "json" };
+
+export default function(eleventyConfig) {
+  eleventyConfig.addFilter("print", JSON.stringify);
+  eleventyConfig.addFilter("asPercent", function(value, total) {
+    if (total === 0) return "0%";
+    return ((value / total) * 100).toFixed(2) + "%";
+  });
+
+  eleventyConfig.addPassthroughCopy({
+    "assets/**": undefined,
+    'node_modules/magick.css/magick.min.css': 'assets/styles/magick.css',
+    'node_modules/normalize.css/normalize.css': 'assets/styles/normalize.css'
+  });
+
+  eleventyConfig.addGlobalData('entriesByYearMonth', function() {
+    return _.chain(entries)
+      .groupBy((entry) => DateTime.fromISO(entry.date).year)
+      .map((yearEntries, year) => [
+        year,
+        _.sortBy(yearEntries, (entry) => -DateTime.fromISO(entry.date))
+      ])
+      .sortBy(([year]) => -year)
+      .value();
+  }());
+
+  eleventyConfig.addGlobalData('stats', function() {
+    const data = {
+      totalEntries: entries.length,
+      entriesByAuthor: _.chain(entries)
+        .countBy('author')
+        .map((count, author) => [author, count])
+        .value(),
+      entriesByYear: _.chain(entries)
+        .countBy((entry) => DateTime.fromISO(entry.date).year)
+        .map((count, year) => [year, count])
+        .value()
+    };
+    console.log({ data });
+    return data;
+  });
+};
